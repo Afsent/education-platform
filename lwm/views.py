4298a05b1bf0da39d8e8719e701adb4e91a58395
Afsent <aleexzhuravlev@gmail.com>
2020-02-05 18:56:44 +0300

    Add login_required

--- lwm/views.py
+++ lwm/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from .models import Lessons
+from django.contrib.auth.decorators import login_required
 
 
+@login_required
 def main(request):
     lessons = Lessons.objects.all()
     return render(request,
