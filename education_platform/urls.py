b7574e7ca1f9042f94a78b2a95fa89eba39fac97
Afsent <aleexzhuravlev@gmail.com>
2020-02-10 17:31:29 +0300

    Update navbar

--- education_platform/urls.py
+++ education_platform/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.contrib.staticfiles.views import serve
+from django.views.decorators.cache import never_cache
 
 urlpatterns = [
-    path('lwm/', include('lwm.urls')),
+    path('lwm/', include('lwm.urls', namespace='')),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns.append(path('static/<path:path>', never_cache(serve)))
