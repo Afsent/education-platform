30e9b9a712ace72512d2198ca62ab4458ee53bf5
Afsent <aleexzhuravlev@gmail.com>
2020-04-05 19:01:30 +0300

    Update group form

--- main/admin.py
+++ main/admin.py
@@ -1,8 +1,9 @@
 import datetime
 
 from django.contrib import admin
+from django.contrib.auth.models import Group
 
-from main.forms import SubRubricForm
+from main.forms import SubRubricForm, GroupAdminForm
 from .utilities import send_activation_notification
 from .models import AdvUser, SubRubric, SuperRubric, AdditionalFile, Lesson, \
     Comment
@@ -100,3 +101,18 @@ class CommentAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Comment, CommentAdmin)
+
+# Unregister the original Group admin.
+admin.site.unregister(Group)
+
+
+# Create a new Group admin.
+class GroupAdmin(admin.ModelAdmin):
+    # Use our custom form.
+    form = GroupAdminForm
+    # Filter permissions horizontal as well.
+    filter_horizontal = ['permissions']
+
+
+# Register the new Group ModelAdmin.
+admin.site.register(Group, GroupAdmin)
